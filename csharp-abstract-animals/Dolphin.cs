d1ea8dbe238c199749f749b683cbbc41a4415b44
Gaspare <Gas9220@users.noreply.github.com>
2023-09-13 14:39:39 +0200

    Test class methods

---
+++ csharp-abstract-animals/Dolphin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Dolphin : Animal
+    {
+        public override void AnimalNoise()
+        {
+            Console.WriteLine("Dolphin sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eating some fish");
+        }
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using csharp_abstract_animals;
+
+Console.WriteLine("Dog test");
+Dog dog = new Dog();
+dog.Sleep();
+dog.AnimalNoise();
+dog.Eat();
+
+Console.WriteLine();
+
+Console.WriteLine("Sparrow test");
+Sparrow sparrow = new Sparrow();
+sparrow.Sleep();
+sparrow.AnimalNoise();
+sparrow.Eat();
+
+Console.WriteLine();
+
+Console.WriteLine("Eagle test");
+Eagle eagle = new Eagle();
+eagle.Sleep();
+eagle.AnimalNoise();
+eagle.Eat();
+
+Console.WriteLine();
+
+Console.WriteLine("Dolphin test");
+Dolphin dolphin = new Dolphin();
+dolphin.Sleep();
+dolphin.AnimalNoise();
+dolphin.Eat();
+
+
