528642a07932b8a274dc0aee67b94cc6dd50c489
Gaspare <Gas9220@users.noreply.github.com>
2023-09-13 14:31:03 +0200

    Sparrow class

---
+++ csharp-abstract-animals/Sparrow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Sparrow : Animal
+    {
+        public override void AnimalNoise()
+        {
+            Console.WriteLine("Cip cip...");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eating worms...");
+        }
+    }
+}
