1087493fa90e3f5a3ce29c180b7ab940ebdd521d
Gaspare <Gas9220@users.noreply.github.com>
2023-09-13 14:33:30 +0200

    Eagle class

---
+++ csharp-abstract-animals/Eagle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Eagle : Animal
+    {
+        public override void AnimalNoise()
+        {
+            Console.WriteLine("Eagle sound");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eating some meat");
+        }
+    }
+}
