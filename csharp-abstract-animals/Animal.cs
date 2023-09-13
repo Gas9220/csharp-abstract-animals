978e1433ed2483ccc3c469324c30fb2e6dbdf90f
Gaspare <Gas9220@users.noreply.github.com>
2023-09-13 14:24:34 +0200

    Aggiungere i file di progetto.

---
+++ csharp-abstract-animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal abstract class Animal
+    {
+        public void Sleep()
+        {
+            Console.WriteLine("Zzzzz....");
+        }
+
+        public abstract void AnimalNoise();
+
+        public abstract void Eat();
+    }
+}
