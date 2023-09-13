4ece6296926b7d610de3869ef739c9055202bad6
Gaspare <Gas9220@users.noreply.github.com>
2023-09-13 14:29:18 +0200

    Dog class

---
+++ csharp-abstract-animals/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Dog: Animal
+    {
+        public override void AnimalNoise()
+        {
+            Console.WriteLine("Bau...");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eating dry dog food...");
+        }
+    }
+}
